8b8d61c2f6036695c8905fe9091dcde6b0058d9e
SissiW <41228546+SissiW@users.noreply.github.com>
2023-10-12 11:59:25 +0800

    Add files via upload

---
+++ train_meta_QSFormer.sh
@@ -0,0 +1,12 @@
+# tieredimagenet
+# python train_meta_QSFormer_tieredimagnet.py -deepemd fcn -dataset tieredimagenet -shot 1 -way 5 -solver opencv -gpu 0
+
+
+# miniimagenet
+# python train_meta_QSFormer_miniimagenet.py -deepemd fcn -dataset miniimagenet -shot 1 -way 5 -solver opencv -gpu 0
+
+# fc100
+# python train_meta_QSFormer_fc100.py -deepemd fcn -dataset fc100 -shot 1 -way 5 -solver opencv -gpu 0 
+
+# cub
+python train_meta_QSFormer_cub.py -deepemd fcn -dataset cub -shot 1 -way 5 -solver opencv -gpu 0 
